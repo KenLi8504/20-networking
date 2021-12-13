293dddc62e85aed1eaaef7dc79cfb39c4873dcf1
KenLi8504 <kli22@stuy.edu>
2021-12-13 13:54:01 -0500

    First attempt at client to server, part 1

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,6 +10,12 @@
   =========================*/
 int server_handshake(int *to_client) {
   int from_client = 0;
+  char *well_known_pipe = "Hey I'm Famous";
+  int pipeto = open(well_known_pipe,O_RDONLY);
+  int *client_PID;
+  read(pipeto,client_PID,8);
+  printf("The PID of the client is %d",*client_PID);
+  //remove(well_known_pipe);
   return from_client;
 }
 
@@ -23,5 +29,8 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   int from_server = 0;
+  char *well_known_pipe = "Hey I'm Famous";
+  int pipeto = open(well_known_pipe,O_WRONLY);
+  write(pipeto,to_server,sizeof(to_server));
   return from_server;
 }
